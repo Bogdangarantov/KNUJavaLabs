c9abc078409e6be67758e864a3109a0c28c2a422
harys <harysh.bogdan@gmail.com>
2022-10-25 01:45:48 +0300

    just formated directories

--- src/src/main/resources/ClassWork/lab1/task1_8/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"org.javapractices.lab1","c":"Task1_8","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"org.javapractices.lab1","c":"Task1_8","l":"Task1_8()","url":"%3Cinit%3E()"}]
